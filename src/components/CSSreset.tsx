c7846766085cdbbbf72901c9e8ed00d3cd103ef5
AhBiel <95438103+AngeloGbiel@users.noreply.github.com>
2023-04-24 15:56:31 -0300

    Update CSSreset.tsx

--- src/components/CSSreset.tsx
+++ src/components/CSSreset.tsx
@@ -1,10 +1,5 @@
 import { createGlobalStyle } from "styled-components";
-interface Props{
-    theme:{
-        photo:string
-    }
-}
-export const CSSreset = createGlobalStyle<Props>`
+export const CSSreset = createGlobalStyle`
     *{
         margin:0;
         padding:0;
@@ -12,14 +7,6 @@ export const CSSreset = createGlobalStyle<Props>`
         font-family: 'Inter', sans-serif;
     }
     body{
-        background-image: url(${({theme})=>theme.photo});
-        background-repeat: no-repeat;
-        background-size: cover;
-        transition: 1s;
-        background-attachment:fixed;
-        @media (max-width:500px){
-            background: #278AFF;
-        }
-        background-position: center;
+        background: #278AFF;
     }
 `
